8af2c8f224a9e25fe7232eeff5969a5a2f5cdd57
tradam <git.tradam.fyi>
2021-08-21 02:42:17 -0400

    added scripts

--- scripts/move.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ./src -type f -exec sed -i 's/\['"$1"'](.*)/['"$1"']('"$2"')/g' {} \;
--- scripts/rename.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ./src -type f -exec sed -i 's/\[.*]('"$2"')/['"$1"']('"$2"')/g' {} \;
